69893fa44136ee9e3cf34a65394d62d99707ca0b
Manuquim <manuelcabreraosorio11@gmail.com>
2023-02-23 20:39:56 +0000

    23-02-23

---
+++ src/front/js/component/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css";
+
+export const Footer = () => {
+    return (
+        <footer className="page-footer font-small text-white bg-dark pt-3">
+            <div className="footer-copyright text-center py-3">
+                <p id="pieDePagina">© 2023 Copyright: <a href="https://www.PetFriends.com">www.PetFriends.com</a></p>  
+            </div>
+        </footer>
+    );
+}
+
--- src/front/js/pages/Home/Home.js
+++ src/front/js/pages/Home/Home.js
@@ -4,6 +4,7 @@ import { NombreInicio } from "./component/NombreInicio/NombreInicio";
 import { SobrePetFriends } from "./component/SobrePetFiends/SobrePetFriends";
 import { Services } from "./component/Servicios/Services";
 
+import { Footer } from "../../component/footer/Footer.js";
 
 export const Home = () => {
   return (
@@ -18,6 +19,10 @@ export const Home = () => {
 
       <div id="servicios">
         <Services />
+      </div>
+
+      <div>
+        <Footer />
       </div>
     </div>
   );
