931940877f1bf642fa93357571057b000def675a
IreneQuero <queroirene@gmail.com>
2023-02-26 23:03:45 +0000

    algunos cambios

--- migrations/versions/a8cd06e267b5_.py
+++ migrations/versions/a8cd06e267b5_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 2f78896ad444
+Revision ID: a8cd06e267b5
 Revises: 
-Create Date: 2023-02-25 11:30:03.715430
+Create Date: 2023-02-26 14:31:53.642897
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2f78896ad444'
+revision = 'a8cd06e267b5'
 down_revision = None
 branch_labels = None
 depends_on = None
