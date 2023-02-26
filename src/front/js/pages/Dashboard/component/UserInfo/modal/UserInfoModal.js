eac68786a9e9bfa15d577ed7127359c3781014fe
pepemiguelx2 <pepemiguelx2@gmail.com>
2023-02-26 14:08:57 +0000

    fix userInfo

--- src/front/js/pages/Dashboard/component/UserInfo/modal/UserInfoModal.js
+++ src/front/js/pages/Dashboard/component/UserInfo/modal/UserInfoModal.js
@@ -1,23 +1,44 @@
 import React from "react";
 import "./userinfomodal.css";
 
-
-export const UserInfoModal = () => {
-  
+export const UserInfoModal = ({ handleOpenModal }) => {
   return (
     <div className="content">
-      <div className="dashboard-box container mt-4 mb-4 p-3 d-flex 
-         flex-column align-items-center bg-secondary col-3">
+      <div
+        className="dashboard-box container mt-4 mb-4 p-3 d-flex 
+         flex-column align-items-center bg-secondary col-3"
+      >
+        <div className="w-100 d-flex justify-content-end">
+          <i
+            className="fa-solid fa-xmark p-2 close-button"
+            onClick={handleOpenModal}
+          ></i>
+        </div>
         <form>
-          <div class="mb-3">
-            <label for="inputName" class="form-label">Nombre</label>
-            <input type="text" class="form-control" id="inputName" aria-describedby="introName"/>
+          <div className="mb-3">
+            <label htmlFor="inputName" className="form-label">
+              Nombre
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputName"
+              aria-describedby="introName"
+            />
           </div>
-          <div class="mb-3">
-            <label for="inputPassword" class="form-label">Password</label>
-            <input type="password" class="form-control" id="inputPassword"/>
+          <div className="mb-3">
+            <label htmlFor="inputPassword" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+            />
           </div>
-          <button type="submit" class="btn btn-dark">Submit</button>
+          <button type="submit" className="btn btn-dark">
+            Submit
+          </button>
         </form>
       </div>
     </div>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,12 +2,12 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       BACKEND_URL:
-        "https://3001-s3rtr3s-petsfriends-q1c1k6wzccl.ws-eu88.gitpod.io/",
-      clientInfo: {}
+        "https://3001-s3rtr3s-petsfriends-7paxawh8nrd.ws-eu88.gitpod.io/",
+      clientInfo: {},
     },
     actions: {
       setClientInfo: () => {
-		    const client = JSON.parse(localStorage.getItem("clientInfo"))
+        const client = JSON.parse(localStorage.getItem("clientInfo"));
         setStore({ clientInfo: client });
       },
     },
