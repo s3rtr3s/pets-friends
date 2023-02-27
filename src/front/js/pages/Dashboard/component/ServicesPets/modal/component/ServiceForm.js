1668b22001c5f2b1f022ed3e82404f5b72ec7379
pepemiguelx2 <pepemiguelx2@gmail.com>
2023-02-27 16:14:47 +0000

    serviceForm

--- src/front/js/pages/Dashboard/component/ServicesPets/modal/component/ServiceForm.js
+++ src/front/js/pages/Dashboard/component/ServicesPets/modal/component/ServiceForm.js
@@ -7,9 +7,26 @@ export const ServiceForm = ({ handleOpenModal }) => {
     title: "",
     price: "",
     description: "",
-    carer_id: store.clientInfo.id
-  })
+    carer_id: store.clientInfo.id,
+  });
 
+  const addService = async () => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(serviceInfo),
+    };
+    const resp = await fetch(`${store.BACKEND_URL}api/services`, options);
+    const data = await resp.json();
+    console.log(data);
+  };
+
+  const handleClick = () => {
+    addService();
+    handleOpenModal();
+  };
 
   return (
     <form className="dashboard-form gap-3 p-3 w-100 d-flex flex-column align-items-center">
@@ -17,13 +34,17 @@ export const ServiceForm = ({ handleOpenModal }) => {
       <input
         className="col-8"
         value={serviceInfo.title}
-        onChange={(e) => setServiceInfo({ ...serviceInfo, title: e.target.value })}
+        onChange={(e) =>
+          setServiceInfo({ ...serviceInfo, title: e.target.value })
+        }
       />
       <label className="fs-5 fw-bold">Precio</label>
       <input
         className="col-8"
         value={serviceInfo.price}
-        onChange={(e) => setServiceInfo({ ...serviceInfo, price: e.target.value })}
+        onChange={(e) =>
+          setServiceInfo({ ...serviceInfo, price: e.target.value })
+        }
       />
       <label className="fs-5 fw-bold">Descripción</label>
       <input
@@ -35,6 +56,7 @@ export const ServiceForm = ({ handleOpenModal }) => {
       />
       <a
         className="btn btn-dark rounded-pill px-3 text-white"
+        onClick={handleClick}
       >
         save
       </a>
