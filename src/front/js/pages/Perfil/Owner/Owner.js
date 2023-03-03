d8745eff4b3379d456fdd9008e0f8fb50dff76cc
Manuquim <manuelcabreraosorio11@gmail.com>
2023-03-03 19:24:07 +0000

    all navbar fixed

--- src/front/js/pages/Perfil/Owner/Owner.js
+++ src/front/js/pages/Perfil/Owner/Owner.js
@@ -34,20 +34,21 @@ export const Owner = () => {
 
 
     return(
-        <div className="container emp-profile">
-        <form method="post">
-            <div className="row">
-                <div className="col-md-4">
-                    <div className="profile-img pt-4">
-                        <img src={owner?.avatar} alt=""/>
+        <div className="distancia">
+            <div className="container emp-profile m-auto">
+                <form method="post">
+                    <div className="row">
+                        <div className="col-md-4">
+                        <div className="profile-img pt-4">
+                            <img src={owner?.avatar} alt=""/>
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="profile-head">
+                    <div className="col-md-6">
+                        <div className="profile-head">
                                 <h2>
                                     {owner?.name} {owner?.surname}
                                 </h2>
-                        <ul className="nav nav-tabs" id="myTab" role="tablist">
+                            <ul className="nav nav-tabs" id="myTab" role="tablist">
                             <li className="nav-item">
                             <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Sobre mí</button>
                             </li>
@@ -57,34 +58,34 @@ export const Owner = () => {
                             <li className="nav-item">
                                 <button className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Galería</button>
                             </li>
-                        </ul>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-4">
-                    <div className="profile-work">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="profile-work">
                         <h6>DATOS DE CONTACTO</h6><br/>
                         <p>Nombre: {owner?.name}</p>
                         <p>Apellidos: {owner?.surname}</p>
                         <p>Email: {owner?.email}</p>
                         <p>Ciudad: {owner?.city}</p>
 
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-8">
-                    <div className="tab-content profile-tab" id="myTabContent">
-                    <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                            <div className="col-md-9">
-                                <div className="bio-content">
-                                    {owner.description}
+                    <div className="col-md-8">
+                        <div className="tab-content profile-tab" id="myTabContent">
+                            <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                                <div className="col-md-9">
+                                    <div className="bio-content">
+                                        {owner.description}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                            <div className="row row-cols-1 row-cols-md-3 g-4">
-                                <div className="d-flex justify-content-start" id="cardspets"> 
-                                    {pets?.map(pet =>(
+                            <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                                <div className="row row-cols-1 row-cols-md-3 g-4">
+                                    <div className="d-flex justify-content-start" id="cardspets"> 
+                                        {pets?.map(pet =>(
                                         <div className="col">   
                                         <div className="ica">
                                             <img src={pet.image} className="card-img-top"></img>
@@ -124,7 +125,8 @@ export const Owner = () => {
                 </div>
             </div>
         
-        </form>           
+        </form> 
+        </div>          
     </div>
     );   
 };
