3fe71491f8324c675c94be7250241e67f2fe8db2
pepemiguelx2 <pepemiguelx2@gmail.com>
2023-03-05 16:22:50 +0000

    userInfoForm

--- src/front/js/pages/Dashboard/component/UserInfo/UserInfo.js
+++ src/front/js/pages/Dashboard/component/UserInfo/UserInfo.js
@@ -40,7 +40,6 @@ export const UserInfo = ({ id }) => {
       >
         Editar Perfil
       </a>
-
       {openModal && <UserInfoModal handleOpenModal={handleOpenModal} getClientInfo={getClientInfo} />}
     </div>
   );
